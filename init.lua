e9b0ca62c76d1aee447423307044ab92e34a7543
Danila Mihailov <danilamihailov@icloud.com>
2024-04-21 23:33:56 +0500

    add more tpopes plugins

--- init.lua
+++ init.lua
@@ -144,6 +144,9 @@ require('lazy').setup({
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   { 'tpope/vim-fugitive', dependencies = 'tpope/vim-dispatch' },
+  'tpope/vim-surround', -- Surrond with braces ysB
+  'tpope/vim-repeat', -- enable repeat for tpope's plugins
+  'tpope/vim-unimpaired', -- ]b for next buffer, ]e for exchange line, etc
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -734,7 +737,7 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      -- require('mini.surround').setup()
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
