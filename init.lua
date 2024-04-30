5c81161ed8c02236d5dbf48af19fe64cc3c0a46c
Danila Mihailov <danilamihailov@icloud.com>
2024-04-30 15:34:36 +0500

    toggle diagnostic virtual_text

--- init.lua
+++ init.lua
@@ -113,12 +113,22 @@ vim.opt.scrolloff = 10
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.g.show_diagnostic_virutal_text = true
+-- Diagnostic keymaps
+vim.diagnostic.config {
+  severity_sort = true,
+  virtual_text = vim.g.show_diagnostic_virutal_text,
+}
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>td', function()
+  vim.g.show_diagnostic_virutal_text = not vim.g.show_diagnostic_virutal_text
+  vim.diagnostic.config { virtual_text = vim.g.show_diagnostic_virutal_text }
+end, { desc = '[T]oggle [D]iagnostics' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
