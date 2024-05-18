6ba6778877542e0de86dcce90e21b0218d0dc01a
Danila Mihailov <danilamihailov@icloud.com>
2024-05-18 22:48:55 +0500

    add treesitter symbol search in telescope

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -85,6 +85,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+    vim.keymap.set('n', '<leader>st', builtin.treesitter, { desc = '[S]earch [T]reesitter' })
     vim.keymap.set('n', '<leader><leader>gc', builtin.git_branches, { desc = '[G]it [C]heckout' })
     vim.keymap.set('n', '<leader><leader>gf', builtin.git_files, { desc = '[G]it [F]iles' })
 
