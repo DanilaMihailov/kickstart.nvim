39e34ed3decef3dd014c7e8dc250ff5a5b3a1e01
Danila Mihailov <danilamihailov@icloud.com>
2024-05-19 23:45:14 +0500

    change difftext color

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -20,6 +20,7 @@ return {
         overrides = {
           LspSignatureActiveParameter = { link = 'Visual' },
           SignColumn = { bg = 'NONE' },
+          DiffText = { bg = gruvbox.palette.faded_blue, fg = 'None' },
           GruvboxRedSign = { bg = 'NONE' },
           GruvboxYellowSign = { bg = 'NONE' },
           GruvboxBlueSign = { bg = 'NONE' },
