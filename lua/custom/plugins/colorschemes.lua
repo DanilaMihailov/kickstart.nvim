5ac8ededfd409bede4cc97d539ff76ce5ec74687
Danila Mihailov <danilamihailov@icloud.com>
2024-05-19 23:20:33 +0500

    fix LspSignatureActiveParameter highlight

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -7,24 +7,18 @@ return {
   },
   { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  {
     'ellisonleao/gruvbox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-
       local gruvbox = require 'gruvbox'
       gruvbox.setup {
         contrast = 'hard',
         -- dim_inactive = false,
         -- transparent_mode = true,
+        inverse = true,
         overrides = {
+          LspSignatureActiveParameter = { link = 'Visual' },
           SignColumn = { bg = 'NONE' },
           GruvboxRedSign = { bg = 'NONE' },
           GruvboxYellowSign = { bg = 'NONE' },
