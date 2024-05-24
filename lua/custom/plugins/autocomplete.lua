97e8107969afe440a5f1f7a97a16c875a43cda8a
Danila Mihailov <danilamihailov@icloud.com>
2024-05-24 12:27:11 +0500

    add angularjs snippets

--- lua/custom/plugins/autocomplete.lua
+++ lua/custom/plugins/autocomplete.lua
@@ -21,6 +21,50 @@ return { -- Autocompletion
         {
           'rafamadriz/friendly-snippets',
           config = function()
+            local ls = require 'luasnip'
+            local t = ls.text_node
+            local s = ls.snippet
+            local i = ls.insert_node
+            local d = ls.dynamic_node
+            local sn = ls.snippet_node
+            ls.add_snippets('javascript', {
+              s({ trig = '$ctrl', name = 'angularjs controller' }, {
+                t [[angular.module('kkrm').controller(']],
+                d(1, function()
+                  local fname = vim.fs.basename(vim.api.nvim_buf_get_name(0))
+                  local splitted = vim.split(fname, '.', { plain = true })
+                  return sn(nil, {
+                    i(1, splitted[1]),
+                  })
+                end),
+                t [[', function ($scope]],
+                i(2, ''),
+                t ') {',
+                t { '', '  ' },
+                i(0),
+                t { '', '' },
+                t '});',
+              }),
+              s({ trig = 'sfn', name = 'scope function' }, {
+                t '$scope.',
+                i(1, 'name'),
+                t ' = function (',
+                i(2, ''),
+                t ') {',
+                t { '', '  ' },
+                i(0),
+                t { '', '' },
+                t '};',
+              }),
+              s({ trig = 'svar', name = 'scope variable' }, {
+                t '$scope.',
+                i(1, 'name'),
+                t ' = ',
+                i(2, 'val'),
+                t ';',
+                i(0),
+              }),
+            })
             require('luasnip.loaders.from_vscode').lazy_load()
           end,
         },
